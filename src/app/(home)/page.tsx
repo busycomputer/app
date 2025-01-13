2345a12a9de5d8a77bfa658db1c55a9980cbc7c8
muadpn <muadpn434@gmail.com>
2025-01-13 10:52:04 +0530

    feat: added home page bounded and moody overlay

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,19 @@
-import { Button, buttonVariants } from '@/components/ui/button'
-import { cn } from '@/lib/utils'
-import Link from 'next/link'
-import React from 'react'
+import Bounded from '@/components/elements/Bounded'
+import HeroTitle from '@/components/hero-title'
+import GreenMoodyOverlay from '@/components/overlay/greeny-moody-overlay'
 
-export default function page() {
+export default function HomePage() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <Link href={'/dashboard/'} className={cn(buttonVariants({ variant: 'secondary' }))}>
-        Dashboard
-      </Link>
-    </div>
+    <Bounded>
+      <div className="flex flex-col items-center justify-center">
+        <HeroTitle
+          TitleFirst="Transform Your Work with"
+          TitleHero="AI Workflow"
+          TitleEnd="Creation and Automation"
+          ShortDesc="Effortlessly design and automate workflows with AI-powered tools. Simplify processes, boost efficiency, and focus on what matters most while letting intelligent automation handle repetitive tasks seamlessly."
+        />
+      </div>
+      <GreenMoodyOverlay TopClassName="top-56 relative" />
+    </Bounded>
   )
 }
---
+++ src/components/navbar/home/home-nav.tsx
@@ -0,0 +1,41 @@
+import Bounded from '@/components/elements/Bounded'
+import { buttonVariants } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import BcLogo from '@/assets/images/logo-192x192.png'
+
+export default function HomeNav() {
+  return (
+    <div className="sticky top-0 border-b bg-background/95 py-1 backdrop-blur-md">
+      <Bounded as={'nav'} className="flex items-center justify-between">
+        <div className="flex items-center justify-center gap-1">
+          <Image
+            className="h-12 w-12 overflow-hidden rounded-full"
+            src={BcLogo}
+            alt="Busy computer logo"
+          />
+          <div>
+            <span className="flex flex-col justify-center text-xs font-bold">
+              <span>Busy</span>
+              <span>Computers</span>
+            </span>
+          </div>
+        </div>
+        <div>
+          <Link
+            className={buttonVariants({
+              variant: 'secondary',
+              size: 'xs',
+              className:
+                'flex items-center justify-center rounded-2xl border p-1.5 px-2.5 text-sm leading-none hover:bg-primary',
+            })}
+            href={'/login'}
+          >
+            Sign in
+          </Link>
+        </div>
+      </Bounded>
+    </div>
+  )
+}
