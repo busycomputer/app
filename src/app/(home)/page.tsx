1614de1a9830102b03d2ec3f975277236cb083f9
muadpn <muadpn434@gmail.com>
2025-01-13 08:39:12 +0530

    feat: added dashboard redirect button on login page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,14 @@
+import { Button, buttonVariants } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>
-      <h1>hello world</h1>
+    <div className="flex min-h-screen items-center justify-center">
+      <Link href={'/dashboard/'} className={cn(buttonVariants({ variant: 'secondary' }))}>
+        Dashboard
+      </Link>
     </div>
   )
 }
