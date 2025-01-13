0cd0e956a7ae3bf0a3ae5386b2e61c209ae8cd04
gorgeousGreen <gorgeousGreen@protonmail.com>
2025-01-13 10:52:04 +0530

    feat: added home page bounded and moody overlay

---
+++ src/components/UtilityUI/page-known-link.tsx
@@ -0,0 +1,40 @@
+'use client'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+import { buttonVariants } from '../ui/button'
+import { cn } from '@/lib/utils'
+
+const PageKnownLink = ({
+  LinkTo,
+  LinkIn,
+  LinkTitle,
+}: {
+  LinkTo: string
+  LinkIn: string
+  LinkTitle: string
+}) => {
+  const pathname = usePathname()
+  const Path = pathname.split('/')
+  const isCurrent = Path.find((p) => p.toLowerCase() === LinkIn.toLowerCase())
+  return (
+    <Link
+      href={LinkTo}
+      className={cn(
+        buttonVariants({
+          variant: `${isCurrent ? 'default' : 'secondary'}`,
+          size: 'lg',
+          className: 'w-full rounded-md p-2 text-center text-lg font-medium tracking-tight',
+        }),
+        {
+          'bg-primary': isCurrent,
+        //   'bg-': !isCurrent, // adjust and Implement
+        }
+      )}
+    >
+      {LinkTitle}
+    </Link>
+  )
+}
+
+export default PageKnownLink
---
+++ src/components/hero-title.tsx
@@ -0,0 +1,29 @@
+import { cn } from '@/lib/utils'
+import React from 'react'
+interface ITopComponent {
+  TitleFirst: string
+  TitleHero: string
+  TitleEnd: string
+  ShortDesc: string
+  descClass?: string
+}
+export default function HeroTitle({
+  TitleFirst,
+  TitleHero,
+  TitleEnd,
+  ShortDesc,
+  descClass,
+}: ITopComponent) {
+  return (
+    <>
+      <div className="mt-12 flex flex-col items-center justify-center text-center">
+        <h1 className="max-w-lg text-3xl  tracking-tight sm:text-4xl">
+          {TitleFirst}
+          <span className=""> {TitleHero} </span>
+          <span className="text-green-500/90">{TitleEnd}</span>
+        </h1>
+        <p className={cn('max-w-prose mt-5 break-words  text-muted-foreground')}>{ShortDesc}</p>
+      </div>
+    </>
+  )
+}
