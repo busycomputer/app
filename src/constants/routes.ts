55a0480ec282f11517cf819892b8e52f0bd9b3b1
muadpn <muadpn434@gmail.com>
2025-01-14 06:59:20 +0530

    fix: reroute authenticated users to dashboard and exit out from auth pages

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,4 +1,4 @@
-export const publicRoutes = ['/auth/sign-in']
-export const authRoutes = ['/auth/sign-in']
+export const publicRoutes = ['/auth/sign-in', '/auth/github','/auth/callback']
+export const authRoutes = ['/auth/sign-in','/auth/github','/auth/callback']
 export const DEFAULT_REDIRECT = '/dashboard'
 export const protectedRoutes = ['/dashboard']
