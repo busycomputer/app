7b4c13df8cb9dfcb909bc41508e4f71646728dab
muadpn <muadpn434@gmail.com>
2025-01-15 08:20:41 +0530

    reverted: postcss.config back to mjs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,7 @@ const { default: flattenColorPalette } = require('tailwindcss/lib/util/flattenCo
 
 const config: Config = {
   darkMode: ['class'],
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       colors: {
