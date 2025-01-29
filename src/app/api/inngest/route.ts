5b7ccda27c5bcf5ee0a719603cb7bc64f471fbd2
aloshy.🅰🅸 <noreply@aloshy.ai>
2025-01-29 14:12:02 +0400

    Add webhook support and enhance `Event` type definition
    Introduced `runOnWebhook` function to handle webhooks and updated the API routes to include it. Enhanced the `Event` type by adding `payload`, `headers`, and `queryParams` fields. Updated dev command to use the latest `inngest-cli` package.

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,10 +1,11 @@
 import { serve } from 'inngest/next'
 import { inngest } from '@/lib/inngest'
 import runOnDemand from '@/lib/inngest/functions/run-on-demand'
+import runOnWebhook from '@/lib/inngest/functions/run-on-webhook'
 
 export const runtime = 'edge'
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [runOnDemand],
+  functions: [runOnDemand, runOnWebhook],
 })
---
+++ src/lib/inngest/functions/run-on-webhook.ts
@@ -0,0 +1,20 @@
+import { Engine } from '@inngest/workflow-kit'
+import { availableActions, inngest } from '@/lib/inngest'
+import { EVENT_RUN_ON_WEBHOOK, FUNCTION_RUN_ON_WEBHOOK } from '@/lib/constants'
+import { loadSupabaseWorkflow } from '@/lib/inngest/loaders/load-supabase-workflow'
+
+const workflowEngine = new Engine({
+  actions: availableActions,
+  loader: loadSupabaseWorkflow,
+})
+
+// Create a function to handle incoming webhooks
+export default inngest.createFunction(
+  { id: FUNCTION_RUN_ON_WEBHOOK },
+  { event: EVENT_RUN_ON_WEBHOOK },
+  async ({ event, step }) => {
+    // When a webhook is received, the transform function in the Inngest dashboard
+    // will convert it to our internal event format before this function runs
+    await workflowEngine.run({ event, step })
+  }
+)
