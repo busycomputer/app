4217ada5bd874d37788763ce4c87ad7e0b3a1461
gorgeousGreen <gorgeousGreen@protonmail.com>
2025-05-25 14:40:20 +0530

    refactor: added responsive layout to hero description section

--- src/components/home/hero/hero-description.tsx
+++ src/components/home/hero/hero-description.tsx
@@ -2,13 +2,16 @@ import StarIcon from '@/components/icons/star-icon'
 
 export default function HeroDescription() {
   return (
-    <div className="mx-auto flex lg:h-72 max-w-screen-xl flex-col items-center justify-between ">
-      <div className="font-space-mono flex h-16 max-w-96 items-center  gap-3 text-sm text-muted-foreground">
-        <span className="h-10 w-10 bg-[#131316]"></span>AUTOMATE, OPTIMIZE, OWN YOUR STACK
+    <div className="mx-auto flex sm:h-60 max-w-screen-xl flex-col items-center  lg:h-72">
+      <div className="font-space-mono mx-3 mb-3 flex max-w-96 shrink-0 items-center gap-3 overflow-hidden border-2 bg-[#070708] px-1 py-1 text-xs font-light text-muted-foreground sm:px-2 sm:py-2 md:text-sm lg:font-normal">
+        <span className="h-8 w-8 shrink-0 bg-[#131316] md:h-10 md:w-10"></span>
+        <p className="line-clamp-1">AUTOMATE, OPTIMIZE, OWN YOUR STACK</p>
       </div>
-      <div className="flex w-full max-w-[884px] flex-col items-center gap-4 ">
-        <div className="font-space text-center text-3xl sm:text-3xl md:text-2xl lg:text-6xl">BUILD AMAZING WORKFLOW AT WARPSEED</div>
-        <div className="font-sans-plex w-full max-w-[630px] text-muted-foreground text-sm lg:text-base">
+      <div className="flex w-full max-w-xl flex-col items-center gap-4  md:max-w-3xl lg:max-w-[884px]">
+        <div className="font-space pt-3 text-center text-3xl sm:text-4xl md:pt-0 md:text-4xl lg:text-[62px] lg:leading-[4.5rem]">
+          BUILD AMAZING WORKFLOW AT WARPSEED
+        </div>
+        <div className="font-sans-plex w-full max-w-[630px] text-center text-xs text-muted-foreground md:text-sm lg:text-[16px] lg:leading-[1.5]">
           Infrastructure that combines powerful AI, DeFi, and data scraping modules with a vibrant
           marketplace where creators can share and monetize their automated solutions
         </div>
