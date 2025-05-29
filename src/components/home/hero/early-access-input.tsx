72f61182c55fe479d8af398b893c085c9cf42a4a
gorgeousGreen <gorgeousGreen@protonmail.com>
2025-05-29 16:06:06 +0530

    chore: Hero section      - Updated box grid layout's animations      - Input box alignment issues      - Navbar responsive design

--- src/components/home/hero/early-access-input.tsx
+++ src/components/home/hero/early-access-input.tsx
@@ -4,17 +4,19 @@ import { Input } from '@/components/ui/input'
 
 export default function EarlyAccessInput() {
   return (
-    <div className="font-space-mono relative mx-auto mb-[70px] mt-3 flex h-9 w-full max-w-80 justify-center sm:max-w-96 md:mt-5 md:h-12 lg:mt-11 lg:h-[60px] lg:max-w-[580px]">
-      <div className="absolute left-0 top-1/2 -translate-x-[calc(100%-13px)] -translate-y-1/2 sm:-translate-x-[calc(100%-4px)] md:-translate-x-[calc(100%+6px)]">
-        <TangledSideArrow className="h-10 w-10 stroke-[#3B3B45] sm:h-20 sm:w-20 md:h-24 md:w-24" />
+    <div className="relative">
+      <div className="font-space-mono relative mb-[70px] mt-32 flex h-9 w-full sm:mx-auto sm:mt-3 sm:max-w-96 sm:justify-center md:mt-5 md:h-12 lg:mt-11 lg:h-[60px] lg:max-w-[580px]">
+        <div className="absolute -top-14 left-14 -translate-x-[calc(100%-13px)] -translate-y-1/2 sm:left-0 sm:top-1/2 sm:-translate-x-[calc(100%-4px)] md:-translate-x-[calc(100%+6px)]">
+          <TangledSideArrow className="h-24 w-24 rotate-90 stroke-[#3B3B45] sm:h-20 sm:w-20 sm:rotate-0 md:h-24 md:w-24" />
+        </div>
+        <Input
+          placeholder="ENTER YOUR EMAIL ADDRESS"
+          className="lg:text[16px] mr-0 h-full w-full max-w-44 border border-white rounded-l-sm rounded-r-none bg-white py-0 text-sm font-bold text-primary placeholder:bg-white placeholder:text-[10px] placeholder:font-thin sm:max-w-56 sm:px-6 sm:py-3 md:max-w-64 md:placeholder:text-xs lg:max-w-96 lg:py-4 lg:leading-[28px] lg:placeholder:text-sm lg:placeholder:tracking-widest xl:placeholder:text-[16px]"
+        />
+        <Button className="-ml-1 h-full w-full max-w-28 rounded-l-none rounded-r-[4px] text-[10px] hover:bg-primary sm:max-w-36 md:max-w-56 md:text-xs lg:text-[16px] xl:max-w-[205px]">
+          GET EARLY ACCESS
+        </Button>
       </div>
-      <Input
-        placeholder="ENTER YOUR EMAIL ADDRESS"
-        className="lg:text[16px] mr-0 h-full w-full max-w-44 rounded-l-sm rounded-r-none bg-white px-6 py-2 text-sm font-bold text-primary placeholder:bg-white placeholder:text-[10px] placeholder:font-thin sm:max-w-56 sm:py-3 md:max-w-64 md:placeholder:text-xs lg:max-w-96 lg:py-4 lg:leading-[28px] lg:placeholder:text-sm lg:placeholder:tracking-widest xl:placeholder:text-[16px]"
-      />
-      <Button className="-ml-1 h-full w-full max-w-28 rounded-l-none rounded-r-[4px] text-[10px] hover:bg-primary sm:max-w-36 md:max-w-56 md:text-xs lg:text-[16px] xl:max-w-[205px]">
-        GET EARLY ACCESS
-      </Button>
     </div>
   )
 }
--- src/components/home/hero/hero.tsx
+++ src/components/home/hero/hero.tsx
@@ -2,20 +2,28 @@ import AnimatedBackgroundSquare from '@/components/svg/animated-background-squar
 import EarlyAccessInput from './early-access-input'
 import HeroDescription from './hero-description'
 import WorkFlowSection from './WorkFlowSection'
+import Bounded from '@/components/elements/Bounded'
 
 export default function Hero() {
   return (
-    <div className="px-3">
-      <div className="relative flex w-full flex-col justify-around pb-10">
-        <div className="absolute left-0 top-0">
-          <AnimatedBackgroundSquare />
-        </div>
-
+    <div className="relative">
+      <div className="absolute left-0 top-0 h-full w-full">
+        <AnimatedBackgroundSquare />
+      </div>
+      <Bounded className="max-w-[1312] px-8 pb-20">
         <div className="bg-circle-gradient absolute -top-[400px] left-1/2 h-[465px] w-64 -translate-x-1/2 rounded-full blur-[400px] md:-top-72 md:w-[365px]" />
         <HeroDescription />
         <EarlyAccessInput />
         <WorkFlowSection />
-      </div>
+      </Bounded>
     </div>
   )
 }
+// <div className="px-3">
+//   <div className="relative flex w-full flex-col justify-around pb-10">
+//     <div className="absolute left-0 top-0 w-full">
+//       <AnimatedBackgroundSquare />
+//     </div>
+
+//   </div>
+// </div>
